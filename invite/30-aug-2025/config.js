7cd7db68793e5c02057420cc588255e9ce04de0e
leos-lab <lab@leoleung.co.uk>
2025-06-10 23:10:45 +0100

    added script.js

--- invite/30-aug-2025/config.js
+++ invite/30-aug-2025/config.js
@@ -1,5 +1,5 @@
 window.COUNTDOWN_CONFIG = {
-  eventDate: "2025-07-26T20:00:00",
+  eventDate: "2025-08-30T20:00:00",
   timeZone: "Europe/London",
   backgroundColor: "#308da9",
   textColor: "#ffe1ae",
